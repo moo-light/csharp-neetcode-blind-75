ee8c6c7a2000db5edb789c100092cfb8026cf5f4
Nguyen Thanh Binh (K16_HCM) <binhntse160860@fpt.edu.vn>
2024-09-15 23:51:16 +0700

    sliding window

---
+++ longest-repeating-substring-with-replacement/Program.cs
@@ -0,0 +1,46 @@
+﻿// find the most length of same character by replacing at most k element 
+int CharacterReplacement(string s, int k)
+{
+    var charsCount = new int[26];
+    var curCharacterMaxCount = 0;
+    int maxLength = 0, l = 0;
+    for (var r = 0; r < s.Length; r++)
+    {
+        charsCount[s[r] - 'A']++;
+
+        curCharacterMaxCount = charsCount.Max(x => x);
+        var mostChangesAllow = r - l + 1 - curCharacterMaxCount;
+        if (mostChangesAllow > k)
+        {
+            charsCount[s[l] - 'A']--;
+            l++; 
+        }
+        maxLength = Math.Max(r-l+1, maxLength);
+
+        //printing
+        for (global::System.Int32 i = 0; i < charsCount.Length; i++)
+        {
+            if (charsCount[i] > 0)
+                Console.Write((char)(i + 'A') + "-" + charsCount[i] + " ");
+        }
+        Console.WriteLine();
+
+        for (global::System.Int32 j = l; j <= r; j++)
+        {
+            Console.Write(s[j]);
+        }
+        Console.WriteLine();
+    }
+
+    return maxLength;
+}
+// ABBCDEEE 2
+// ABB -> AAA
+// ABBC x
+// BBCD -> BBBB
+// CDEE -> EEEE
+// CDEEE
+
+Console.WriteLine(CharacterReplacement("XYYX", 2));
+Console.WriteLine(CharacterReplacement("AAABABB", 1));
+Console.WriteLine(CharacterReplacement("XYYXSAAADDAWREE", 2));
---
+++ max-water-container/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public static int MaxArea(int[] heights)
+    {
+        int l = 0, r = heights.Length-1;
+        int maxArea = 0;
+        do
+        {
+            var area = CalculateArea(l, r, heights);
+            maxArea = Math.Max(area, maxArea);
+            if (heights[r] > heights[l])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        } while (l < r);
+        return maxArea;
+    }
+    public static int CalculateArea(int l, int r, int[] heights)
+    {
+
+        return (r - l) * Math.Min(heights[l], heights[r]);
+    }
+    public static void Main()
+    {
+        Console.WriteLine(MaxArea(new int[] { 1, 7, 2, 5, 4, 7, 3, 6 }));
+    }
+}
