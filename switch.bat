410315bc407f3b36d5e9476d026cedfa1058d2a6
NgThanhBình <98044466+moo-light@users.noreply.github.com>
2024-09-24 15:41:16 +0700

    Delete switch.bat

--- switch.bat
+++
@@ -1,4 +0,0 @@
-git config --global user.name "moolight"
-git config --global user.email "binhnguyenthanh19242yahoo@gmail.com"
-git remote set-url origin git@github.com:moo-light/csharp-neetcode-blind-75.git
-git config --list
