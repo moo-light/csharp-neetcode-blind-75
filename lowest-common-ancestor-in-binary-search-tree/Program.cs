729b830ddcea5d582655b752643ea98a0cb037b6
moolight <binhnguyenthanh19242yahoo@gmail.com>
2024-09-26 15:03:19 +0700

    Lowest common ancestor

---
+++ lowest-common-ancestor-in-binary-search-tree/Program.cs
@@ -0,0 +1,16 @@
+﻿using Utils.Tree;
+
+public class Solution
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+
+    }
+
+    public static void Main(string[] args)
+    {
+        var sol = new Solution();
+        sol.LowestCommonAncestor(TreeNode.CreateTree(new List<int?> { 5, 3, 8, 1, 4, 7, 9, null, 2 }), new TreeNode(3), new TreeNode(8)).Print();
+        sol.LowestCommonAncestor(TreeNode.CreateTree(new List<int?> { 5, 3, 8, 1, 4, 7, 9, null, 2}), new TreeNode(3), new TreeNode(4)).Print();
+    }
+}
