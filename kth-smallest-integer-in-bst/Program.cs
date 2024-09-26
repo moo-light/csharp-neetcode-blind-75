699736dece67d2dd4f4e3a0afbe142dcf8fa5870
moolight <binhnguyenthanh19242yahoo@gmail.com>
2024-09-26 17:57:25 +0700

    is binary search

---
+++ kth-smallest-integer-in-bst/Program.cs
@@ -0,0 +1,14 @@
+﻿using Utils.Tree;
+
+public class Solution
+{
+    public int KthSmallest(TreeNode root, int k)
+    {
+        return 1;
+    }
+    public static void Main(string[] args)
+    {
+        var sol = new Solution();
+        sol.KthSmallest(new TreeNode(new int[] { 2, 1, 3 }).Print(), 1);
+    }
+}
