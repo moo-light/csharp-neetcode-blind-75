2b36783e7b483dd22c15419ae9e7a40287c2d50c
moolight <binhnguyenthanh19242yahoo@gmail.com>
2024-09-26 18:02:12 +0700

    is valid binary search tree

---
+++ level-order-traversal-of-binary-tree/Program.cs
@@ -0,0 +1,42 @@
+﻿using Utils.Tree;
+
+public class Solution
+{
+
+    public List<List<int>> LevelOrder(TreeNode root)
+    {
+        var res = new List<List<int>>();
+        var queue = new Queue<TreeNode>();
+        if(root !=null) queue.Enqueue(root);
+        while (queue.Count > 0)
+        {
+            var list = new List<int>();
+            var n = queue.Count;
+            for (int i = 0; i < n; i++)
+            {
+                var node = queue.Dequeue();
+                // change from node.val.Value to node.val to be able to run in neetcode
+                list.Add(node.val.Value); 
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+            res.Add(list);
+        }
+
+        return res;
+    }
+
+    public static void Main(string[] args)
+    {
+        var sol = new Solution();
+        TreeNode root = TreeNode.CreateTree(new List<int?> { 1, 2, 3, 4, 5, 6, 7 });
+        root.Print();
+        sol.LevelOrder(root).Print();
+    }
+}
---
+++ valid-binary-search-tree/Program.cs
@@ -0,0 +1,36 @@
+﻿using Utils.Tree;
+
+public class Solution
+{
+    //dfs
+    /* find if Tree is valid binary search tree
+     * condition: left < mid <right and left is also bst
+     * First: check left < mid and right > mid
+     * Second: check left and right is BST
+     * IF: left == null || right == null return true because there's nothing to check
+     */
+    public bool IsValidBST(TreeNode root)
+    {
+        if (root == null) return true;
+        return Valid(root, int.MinValue,int.MaxValue);
+    }
+    public bool Valid(TreeNode node, int min, int max)
+    {
+        if (node == null) return true;
+        if (!(min < node.val && node.val < max))
+        {
+            return false;
+        }
+
+        return Valid(node.left, min, node.val)&&Valid(node.right, node.val,max);
+    }
+    //bfs if wanted
+    public static void Main(string[] args)
+    {
+        var sol = new Solution();
+        sol.IsValidBST(TreeNode.CreateTree(new List<int?> { 2,1, 3 })).Print();
+        TreeNode root = TreeNode.CreateTree(new List<int?> { 5, 4, 6, null, null, 3, 7 });
+        root.Print();
+        sol.IsValidBST(root).Print();
+    }
+}
