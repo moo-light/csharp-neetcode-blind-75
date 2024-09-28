cb60a23da74e4f41d17aa16bb430e4f0787dc090
moolight <binhnguyenthanh19242yahoo@gmail.com>
2024-09-28 22:41:32 +0700

    kth smallest in BST

--- kth-smallest-integer-in-bst/Program.cs
+++ kth-smallest-integer-in-bst/Program.cs
@@ -4,11 +4,51 @@ public class Solution
 {
     public int KthSmallest(TreeNode root, int k)
     {
-        return 1;
+        //var list = new List<int>();
+        //DFS(root, list, k);
+        //list.Print();
+        //return list[k-1];
+        return FindKNeetCodeSolution(root, k);
     }
+    // should find better solution
+    private void DFS(TreeNode curr, List<int> list, int k)
+    {
+        if (curr == null) return;
+        DFS(curr.left, list, k);
+        list.Add(curr.val);
+        DFS(curr.right, list, k);
+    }
+
+    //this solution will find K instantly and  not wait for the DFS to finish
+    int FindKNeetCodeSolution(TreeNode root, int k)
+    {
+        var st = new Stack<TreeNode>();
+        var curr = root;
+        while (curr != null ||st.Count>0)
+        {
+            //traversal to the left until end of curr
+            //this will not have null in stack
+            while (curr != null)
+            {
+                st.Push(curr);
+                curr = curr.left;
+            }
+            curr = st.Pop();//pop every loop
+            k--;
+            if (k == 0) return curr.val;
+            //curr can be null but if stack still have number it will continue
+            curr = curr.right;
+        }
+        return -1;
+    }
+
+
+
     public static void Main(string[] args)
     {
         var sol = new Solution();
-        sol.KthSmallest(new TreeNode(new int[] { 2, 1, 3 }).Print(), 1);
+        sol.KthSmallest(new TreeNode(new int[] { 2, 1, 3 }).Print(), 1).Print();
+        sol.KthSmallest(new TreeNode(new List<int?> { 4, 3, 5, 2, null }).Print(), 4).Print();
+        sol.KthSmallest(new TreeNode(new List<int?> { 5, 2, 8, 1, 4, 6, 9, null, null, 3, null, null, 7 }).Print(), 4).Print();
     }
 }
